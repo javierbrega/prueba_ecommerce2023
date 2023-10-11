8fd25e0fe40c9a299f26d9600c750b658799633d
JAVIER BREGA <javierbrega@gmail.com>
2023-10-11 00:36:56 -0300

    Update registro.js
    modificando registro

--- routes/registro.js
+++ routes/registro.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose(); // Importa la librería SQLite
 
-// Ruta para mostrar el formulario de registro
+const db = new sqlite3.Database('./data/eCommerce.db'); // Conexión a la base de datos
+
 router.get('/', (req, res) => {
-  res.render('registro'); 
+  res.render('registro'); // Renderiza la vista de registro
 });
 
-// Ruta para procesar los datos del formulario de registro
 router.post('/registrar', (req, res) => {
-  // Aquí puedes agregar la lógica para registrar a los usuarios en tu base de datos.
-  // Esto puede incluir validación de datos, creación de usuarios en la base de datos, etc.
+  const { username, password } = req.body;
 
-  // Redirige a la página de inicio o a donde desees después del registro.
-  res.redirect('/');
+  // Valida que se hayan proporcionado tanto un nombre de usuario como una contraseña.
+  if (!username || !password) {
+    return res.render('registro', { error: 'Se requiere nombre de usuario y contraseña' });
+  }
+
+  // Comprueba si el nombre de usuario ya está en uso en la base de datos.
+  const query = 'SELECT * FROM usuarios WHERE username = ?';
+
+  db.get(query, [username], (err, row) => {
+    if (err) {
+      return res.render('registro', { error: 'Error al registrar el usuario' });
+    }
+
+    if (row) {
+      return res.render('registro', { error: 'El nombre de usuario ya está en uso' });
+    }
+
+    // Si el nombre de usuario no está en uso, crea un nuevo usuario en la base de datos.
+    const insertQuery = 'INSERT INTO usuarios (username, password) VALUES (?, ?)';
+
+    db.run(insertQuery, [username, password], (err) => {
+      if (err) {
+        return res.render('registro', { error: 'Error al registrar el usuario' });
+      }
+
+      // Redirige a la página de inicio o a donde desees después del registro exitoso.
+      res.redirect('/');
+    });
+  });
 });
 
 module.exports = router;
